997bb2e6fb14aa668a6d0d1fd09edf2d048a2603
edrueda@stcsolutions.com.ve <edrueda@stcsolutions.com.ve>
2014-12-11 13:55:12 -0430

    ajustes en vista de coleccciones

--- app/views/collections.blade.php
+++ app/views/collections.blade.php
@@ -3,25 +3,27 @@
 @section('content')
 <div class="container content">
     @foreach($collection->collectionsType as $type)
-    <div class="row">
-        <div class="col-md-1">
-            <div class="category-title">
-                <p>{{ $type->title }}</p>
-            </div>
-        </div>
-        <div class="col-md-11">
-            <div class="row">
-                @foreach($type->productos as $prod)
-                <div class="col-md-3 text-center">
-                    <a class="ring" href="#">
-                        <img src="{{ asset('uploads/productos') }}/{{ $prod->img }}" alt="{{ $prod->title }}" height="80">
-                    </a>
+        @if($type->productos)
+        <div class="row">
+            <div class="col-md-1">
+                <div class="category-title">
+                    <p>{{ $type->title }}</p>
                 </div>
-                @endforeach
+            </div>
+            <div class="col-md-11">
+                <div class="row">
+                    @foreach($type->productos as $prod)
+                    <div class="col-md-3 text-center">
+                        <a class="ring" href="#">
+                            <img src="{{ asset('uploads/productos') }}/{{ $prod->img }}" alt="{{ $prod->title }}" height="180">
+                        </a>
+                    </div>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-    <img class="hr" src="{{ asset('img/hr.png') }}" alt="">
+        <img class="hr" src="{{ asset('img/hr.png') }}" alt="">
+        @endif
     @endforeach
 </div>
 @endsection
